de9fbf6e991c8c31fe5602f8e63ed22a618e9ee2
pfjhetg <pfjhetg@qq.com>
2021-01-15 23:15:20 +0800

    创建实例解析；

--- LearnVulkan/Source Files/Main.cpp
+++ LearnVulkan/Source Files/Main.cpp
@@ -13,6 +13,7 @@ public:
 	void run() {
 		// 初始化GLFW
 		initWindow();
+		// 初始化Vulkan
 		initVulkan();
 		mainLoop();
 		cleanup();
@@ -20,6 +21,7 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
 	void initWindow() {
 		glfwInit();
@@ -33,7 +35,7 @@ private:
 	}
 
 	void initVulkan() {
-
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -43,9 +45,48 @@ private:
 	}
 
 	void cleanup() {
+		// 这个实例在清理的时候需要自己销毁
+		vkDestroyInstance(instance, nullptr);
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
+	}
+
+	// 创建Vulkan实例，用于连接应用和Vulkan库
+	void createInstance() {
+		VkInstanceCreateInfo createInfo{};
+		// 可以从这个方法作用域尾部的代码开始往前读这理解
+		// 1.要创建一个实例，需要指定一些列参数
+		// 2.可用层暂时不做考虑，后面补充
+		// 3.扩展数量和对应的扩展名字是通过上面部分专门的方法获取（vulkan很多地方都是通过这样类似方式获取的，一定要熟悉这种方式）
+		// 4.pApplicationInfo参数需要一个新的对象，因此就有了代码最前面部分的VkApplicationInfo
+
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+	
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char **glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		// 最后创建实例
+		// 第一个参数就是我们这里用到的创建信息，第二个是回调，这里不关注留空，第三个就是创建后的Handle
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
 	}
 };
 
